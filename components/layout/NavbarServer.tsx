466bd24ea3540e8b67a70981df99e79521b58695
Umesh Verma <rajumesh8969457707@gmail.com>
2026-02-11 16:40:18 +0530

    Removed JSX.Element type from NavbarServer

--- components/layout/NavbarServer.tsx
+++ components/layout/NavbarServer.tsx
@@ -3,7 +3,8 @@ import { announcementQuery } from "@/lib/sanity.queries";
 import NavbarClient from "./NavbarClient";
 
 export default async function NavbarServer() {
-  const announcements = await sanityClient.fetch(announcementQuery);
+  const announcements =
+    (await sanityClient.fetch(announcementQuery)) ?? [];
 
-  return <NavbarClient announcements={announcements ?? []} />;
+  return <NavbarClient announcements={announcements} />;
 }
