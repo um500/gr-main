25d351787f6e868076aa85469f22793090d2af1e
Umesh <rajumesh8969457707@gmail.com>
2026-02-12 22:37:57 +0530

    First commit

--- lib/sanity.client.ts
+++ lib/sanity.client.ts
@@ -3,7 +3,6 @@ import { createClient } from "next-sanity";
 export const sanityClient = createClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
-  apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION!,
+  apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2024-01-01",
   useCdn: false,
 });
-  
